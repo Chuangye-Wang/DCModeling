d38d61ff705b25f6b020d928ddecc05854eb53e0
Chuangye Wang <cwang861@gatech.edu>
2023-06-15 14:16:26 -0400

    update figures

---
+++ constants.py
@@ -0,0 +1,6 @@
+class Constant:
+    """ To provides common constants values for calculations.
+    """
+    def __init__(self):
+        self.R = 8.3145  # unit: J/mol/K
+        self.T0 = 273  # unit: K
